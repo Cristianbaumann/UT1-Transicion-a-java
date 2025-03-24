7f4b1efb4e9bc83c6544fde6716da62d9745c31d
Cristianbaumann <cristian.baumann@correo.ucu.edu.uy>
2025-03-24 18:48:59 -0300

    ut1_ta3_eje1

---
+++ ut1_ta3_eje1/src/main/java/com/example/Main.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        ContadorPalabras contador = new ContadorPalabras();
+        System.out.println(contador.contadorPalabras("si piensas que ti profesor es exigente,.. espera a conocer a tu jefe !*"));
+    }
+}
